8e811a1457044ac86656a041a594a4251f0f93c3
AhamedAliNishad <ahamedalinishad.j@syncfusion.com>
2022-09-26 11:38:11 +0530

    AvatarView-Clipping Issue

---
+++ BorderClipImage/BorderClipImage/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BorderClipImage;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
